1a5424d100aec6272d916433be7b414bd14c3f9e
Alex <alexalbuquerqueti@gmail.com>
2025-01-26 15:22:18 -0300

    desafio do código

---
+++ controleSaque/src/ControleSimplesDeSaques.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class ControleSimplesDeSaques {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Informe o valor do limite diario de saque:");
+
+        double limiteDiario = scanner.nextDouble();
+
+        // Loop for para iterar sobre os saques
+        for (int i = 1; ; i++) {
+
+            System.out.println("Digite o valor do saque:");
+            double valorSaque = scanner.nextDouble();
+
+            if (valorSaque == 0) {
+                System.out.println("Transacoes encerradas.");
+                break; // Encerra o loop
+            } else if (valorSaque > limiteDiario) {
+                System.out.println("Limite diario de saque atingido. Transacoes encerradas.");
+                break; // Encerra o loop
+            } else {
+                // TODO: Atualizar o limite diário e imprimir a saída no formato dos exemplos.
+                limiteDiario -= valorSaque;
+                System.out.println("Saque realizado. Limite restante: " + limiteDiario);
+            }
+        }
+
+        // Fechar o scanner para evitar vazamentos de recursos
+        scanner.close();
+    }
+}
